c0d2c1b5d1254eda683b6707222a1231202b10b2
Lucas Machado <73367620+lucsmachado@users.noreply.github.com>
2022-03-02 17:34:19 -0300

    step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   { 
     "id": 1,
@@ -52,7 +54,33 @@ app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter(person => person.id !== id);
   response.status(204).end();
-})
+});
+
+const generateId = () => {
+  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+};
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response
+      .status(400)
+      .json(
+        {
+          error: 'Missing properties \'name\' and / or \'number\' of person'
+        }
+      );
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  };
+  persons = persons.concat(person);
+  response.json(person);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
