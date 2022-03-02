af244d612393f4df0714ad755ace589740394b35
Lucas Machado <73367620+lucsmachado@users.noreply.github.com>
2022-03-02 16:57:22 -0300

    step3

--- index.js
+++ index.js
@@ -36,6 +36,18 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (!person) {
+    response.statusMessage = `Person with id=${id} not found`;
+    return response.status(404).end();
+  }
+
+  response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
