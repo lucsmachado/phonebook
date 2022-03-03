e5c2c43aa8d065c1f8ea5ca2871d2f93bc72c710
Lucas Machado <73367620+lucsmachado@users.noreply.github.com>
2022-03-03 16:43:29 -0300

    step10

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 app.use(morgan((tokens, request, response) => {
   const format = [
     tokens.method(request, response),
